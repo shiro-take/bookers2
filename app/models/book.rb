5ba8e898c6cd0c0ee28f5d288b420304517e6bef
EC2 Default User <ec2-user@ip-172-31-21-223.ec2.internal>
2022-11-08 10:01:58 +0000

    [Add]favorite

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
   validates :body,  presence: true,
                     length: {maximum: 200}
                     
-  def favorited_by?
+  def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
 
