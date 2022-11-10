2875df87a4f6ad35d22f81387110b4cfaa1d368f
EC2 Default User <ec2-user@ip-172-31-21-223.ec2.internal>
2022-11-10 14:16:35 +0000

    [Finish]bookers2 + α

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,7 +7,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-     book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_to request.referer
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
